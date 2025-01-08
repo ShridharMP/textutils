e198e47583e3c6eb2c18c2201ada0bdf74208c3d
ShridharMP <mpshridhar2016@gmail.com>
2025-01-08 11:29:25 +0530

    code changes

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,17 +1,11 @@
 import React from "react";
-function Alert({alert}) {
+function Alert(props) {
   return (
-    <div>
-      <div className={`alert alert-${alert.type} alert-dismissible fade show`} role="alert">
-        {alert.msg}
-        {/* <button
-          type="button"
-          className="btn-close"
-          data-bs-dismiss="alert"
-          aria-label="Close"
-        ></button> */}
+    props.alert && (
+      <div className={`alert alert-${props.alert.type} alert-dismissible fade show`} role="alert">
+        {props.alert.msg}
       </div>
-    </div>
+    )
   );
 }
 export default Alert;
